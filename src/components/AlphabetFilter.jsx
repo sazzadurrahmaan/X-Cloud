4c796bc96cc2a4c81890615f3e56e6594e2e9b9e
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-12-25 14:41:25 +0600

    name showing over alphabet tiles

--- src/components/AlphabetFilter.jsx
+++ src/components/AlphabetFilter.jsx
@@ -23,32 +23,39 @@ const AlphabetFilter = ({ activeLetter, onFilterChange }) => {
         const { bg, text } = colorConfig[index % colorConfig.length];
 
         return (
-          <button
-            key={char}
-            onClick={() => onFilterChange(char)}
-            className={`relative w-[50px] h-[50px] text-[24px]  font-semibold leading-[24px] rounded-[8px] 
-            ${bg} ${text} transform transition-transform duration-200 
-            ${char === "M" ? "-translate-y-3" : "hover:-translate-y-3"}`}
-          >
-            {char}
-            {/* Icons */}
-            <span
-              className={`absolute top-[-4px] right-[-4px] rounded-full p-1 transition-opacity duration-200 ${
-                char === "M"
-                  ? "bg-white text-red-500 border border-black opacity-100"
-                  : "bg-green-500 text-white hover:opacity-0"
-              }`}
+          <div className="relative group" key={char}>
+            {/* Tooltip */}
+            {char === "M" && (
+              <div className="absolute -top-12 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[6px] border-b-[#1A1D3A]  text-nowrap left-[1/2] transform -translate-x-1/3 bg-[#232A4E] text-white text-xs px-4 py-2 rounded-md opacity-100 group-hover:opacity-100 transition-opacity duration-200">
+                Plugin Name
+              </div>
+            )}
+            <button
+              onClick={() => onFilterChange(char)}
+              className={`relative w-[50px] h-[50px] text-[24px] font-semibold leading-[24px] rounded-[8px] 
+              ${bg} ${text} transform transition-transform duration-200 
+              ${char === "M" ? "-translate-y-3" : "hover:-translate-y-3"}`}
             >
-              {char === "M" ? <FaTimes className="h-2 w-2" /> : <FaCheck className="h-2 w-2" />}
-            </span>
-            <span
-              className={`absolute top-[-4px] right-[-4px] bg-white text-red-500 border border-black  rounded-full p-1 opacity-0 transition-opacity duration-200 ${
-                char === "M" ? "opacity-0" : "hover:opacity-100"
-              }`}
-            >
-              <FaTimes className="h-2 w-2" />
-            </span>
-          </button>
+              {char}
+              {/* Icons */}
+              <span
+                className={`absolute top-[-4px] right-[-4px] rounded-full p-1 transition-opacity duration-200 ${
+                  char === "M"
+                    ? "bg-white text-red-500 border border-black opacity-100"
+                    : "bg-green-500 text-white hover:opacity-0"
+                }`}
+              >
+                {char === "M" ? <FaTimes className="h-2 w-2" /> : <FaCheck className="h-2 w-2" />}
+              </span>
+              <span
+                className={`absolute top-[-4px] right-[-4px] bg-white text-red-500 border border-black rounded-full p-1 opacity-0 transition-opacity duration-200 ${
+                  char === "M" ? "opacity-0" : "hover:opacity-100"
+                }`}
+              >
+                <FaTimes className="h-2 w-2" />
+              </span>
+            </button>
+          </div>
         );
       })}
     </div>
