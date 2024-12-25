d6131776d3473d698b9d22806f11eb29470bb7f9
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-12-25 17:23:27 +0600

    navigating to create blue print from all view

--- src/pageComponents/AllBluePrints.jsx
+++ src/pageComponents/AllBluePrints.jsx
@@ -1,15 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import BlueprintCard from "../components/BlueprintCard";
 import blueprintsData from "../data/blueprintData";
 
 
 const AllBluePrints = () => {
     const blueprints = blueprintsData()
-    
+    const navigate = useNavigate();
       return (
         <div className=" w-full max-w-5xl bg-[#1D2239]  rounded-lg mx-auto shadow-lg p-6 text-white">
           <div className="flex justify-between items-center mb-6">
             <h1 className="text-[32px] leading-[35px] font-medium">All Blueprint</h1>
-            <button className="bg-[#147AFF] text-[14px] leading-[16px] font-semibold text-white py-2 px-4 rounded-[4px] hover:bg-[#0D6FD1] transition">
+            <button onClick={() => navigate("/create-blueprint")} className="bg-[#147AFF] text-[14px] leading-[16px] font-semibold text-white py-2 px-4 rounded-[4px] hover:bg-[#0D6FD1] transition">
               + Create New Blueprint
             </button>
           </div>
