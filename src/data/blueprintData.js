1bae99560b1d26694f14b2d0ad94431fbde4405c
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-12-25 18:57:24 +0600

    fixed the all view height issue and added some more data

--- src/data/blueprintData.js
+++ src/data/blueprintData.js
@@ -1,75 +1,97 @@
 const blueprints = [
-    { name: "Blueprint Name One", isDefault: true, tags: [
+    { name: "Blueprint Name - One", isDefault: true, tags: [
       {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
       {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
       {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
       {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
     
     ] },
-    { name: "Blueprint Name One", isDefault: true, tags: [
+    { name: "Blueprint Name - 2", isDefault: true, tags: [
       {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
       {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
       {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
       {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
     
     ] },
-    { name: "Blueprint Name One", isDefault: true, tags: [
+    { name: "Blueprint Name - 3", isDefault: true, tags: [
       {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
       {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
       {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
       {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
     
     ] },
-    { name: "Blueprint Name One", isDefault: true, tags: [
+    { name: "Blueprint Name - 4", isDefault: true, tags: [
       {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
       {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
       {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
       {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
     
     ] },
-    { name: "Blueprint Name One", isDefault: true, tags: [
+    { name: "Blueprint Name - 5", isDefault: true, tags: [
         {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
         {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
         {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
         {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
       
-      ] },    { name: "Blueprint Name One", isDefault: true, tags: [
+      ] },    { name: "Blueprint Name - 6", isDefault: true, tags: [
         {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
         {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
         {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
         {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
       
-      ] },    { name: "Blueprint Name One", isDefault: true, tags: [
+      ] },    { name: "Blueprint Name - 7", isDefault: true, tags: [
         {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
         {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
         {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
         {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
       
-      ] },    { name: "Blueprint Name One", isDefault: true, tags: [
+      ] },    { name: "Blueprint Name - 8", isDefault: true, tags: [
         {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
         {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
         {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
         {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
       
-      ] },    { name: "Blueprint Name One", isDefault: true, tags: [
+      ] },    { name: "Blueprint Name - 9", isDefault: true, tags: [
         {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
         {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
         {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
         {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
       
-      ] },    { name: "Blueprint Name One", isDefault: true, tags: [
+      ] },    { name: "Blueprint Name - 10", isDefault: true, tags: [
         {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
         {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
         {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
         {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
       
-      ] },    { name: "Blueprint Name One", isDefault: true, tags: [
+      ] },    { name: "Blueprint Name - 11", isDefault: true, tags: [
         {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
         {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
         {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
         {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
       
       ] },
+      { name: "Blueprint Name - 12", isDefault: true, tags: [
+        {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
+        {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
+        {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
+        {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
+      
+      ] },
+      { name: "Blueprint Name - 13", isDefault: true, tags: [
+        {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
+        {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
+        {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
+        {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
+      
+      ] },
+      { name: "Blueprint Name - 14", isDefault: true, tags: [
+        {ch:"A",bg: "bg-[#CCE6FF]", text: "text-[#6C90B4]"}, 
+        {ch:"N",bg: "bg-[#FFE2A4]", text: "text-[#CAA452]" },
+        {ch:"X", bg: "bg-[#FFC4C4]", text: "text-[#EC6F6F]"  },
+        {ch:"4+", bg: "bg-[#80B7FF]", text: "text-[#3784E8]" },
+      
+      ] },
+
   ];
 
   const blueprintsData = () =>{
